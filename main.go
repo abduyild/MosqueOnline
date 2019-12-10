09bef6758bce762e701299c960727c9a5ef90011
Abdullah Yildirim <vk840@fzi..de>
2019-12-10 22:22:03 +0100

    Initialize

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+ 
+import (
+    "net/http"
+ 
+    "github.com/gorilla/mux"
+ 
+    common "./common"
+)
+ 
+var router = mux.NewRouter()
+ 
+func main() {
+    router.HandleFunc("/", common.LoginPageHandler) // GET
+    router.HandleFunc("/login", common.LoginHandler).Methods("POST")
+    
+    router.HandleFunc("/index", common.IndexPageHandler) // GET
+    
+    router.HandleFunc("/register", common.RegisterPageHandler).Methods("GET")
+    router.HandleFunc("/register", common.RegisterHandler).Methods("POST")
+    
+    router.HandleFunc("/logout", common.LogoutHandler)//.Methods("POST")
+	router.HandleFunc("/reset", common.ResetHandler)
+	router.HandleFunc("/submit", common.SubmitHandler)
+	router.HandleFunc("/appleHeadquarter", common.SteveJobsHandler)
+	
+    http.Handle("/", router)
+    
+
+    http.ListenAndServe(":8080", nil)
+}
