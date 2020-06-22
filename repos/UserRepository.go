0e0bbba368d7ec6cf6368da040f5737f58ee8427
Abdullah Yildirim <yildabdu@gmail.com>
2020-06-22 23:02:13 +0200

    Crypto now working properly

--- repos/UserRepository.go
+++ repos/UserRepository.go
@@ -4,11 +4,7 @@ import (
 	"context"
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/rand"
 	b64 "encoding/base64"
-	"encoding/pem"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"pi-software/model"
 	"strings"
@@ -20,9 +16,9 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const (
-	keyFile = "aes.key"
-)
+const keyFile = "aes.key"
+
+var key = []byte{11, 108, 111, 57, 116, 83, 193, 127, 59, 57, 245, 188, 171, 59, 187, 101}
 
 var IV = []byte("1234567812345678")
 
@@ -60,48 +56,8 @@ func GetDBCollection(i int) (*mongo.Collection, error) {
 	return nil, nil
 }
 
-func readKey(filename string) ([]byte, error) {
-	key, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return key, err
-	}
-	block, _ := pem.Decode(key)
-	return block.Bytes, nil
-}
-
-func createKey() []byte {
-	genkey := make([]byte, 16)
-	_, err := rand.Read(genkey)
-	if err != nil {
-		log.Fatalf("Failed to read new random key: %s", err)
-	}
-	return genkey
-}
-
-func saveKey(filename string, key []byte) {
-	block := &pem.Block{
-		Type:  "AES KEY",
-		Bytes: key,
-	}
-	err := ioutil.WriteFile(filename, pem.EncodeToMemory(block), 0644)
-	if err != nil {
-		log.Fatalf("Failed in saving key to %s: %s", filename, err)
-	}
-}
-
-func aesKey() []byte {
-	file := fmt.Sprintf(keyFile)
-	key, err := readKey(file)
-	if err != nil {
-		log.Println("Creating a new AES key")
-		key = createKey()
-		saveKey(file, key)
-	}
-	return key
-}
-
 func createCipher() cipher.Block {
-	c, err := aes.NewCipher(aesKey())
+	c, err := aes.NewCipher(key)
 	if err != nil {
 		log.Fatalf("Failed to create the AES cipher: %s", err)
 	}
